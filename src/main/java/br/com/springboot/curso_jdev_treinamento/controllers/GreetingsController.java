1e22d9e7e55aa5fd325632c076d05d5936db12a2
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-09-26 10:08:05 -0300

    Criando END-POINT para buscar e atualizar no banco

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,22 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
 @RestController
 public class GreetingsController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     *
-     */
     @RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
@@ -54,7 +45,7 @@ public class GreetingsController {
     	return "Ola mundo " + nome;
     }
     
-    @GetMapping(value = "listartodos")
+    @GetMapping(value = "/listartodos")
     @ResponseBody /*Retorna os dados para o corpo da resposta*/
     public ResponseEntity<List<Usuario>> listaUsuario() {
     	
@@ -79,5 +70,29 @@ public class GreetingsController {
        usuarioRepository.deleteById(idUser);
        
        return new ResponseEntity<String>("User Deletado com Sucesso", HttpStatus.OK);
+    }
+    
+    
+    @GetMapping(value = "buscaruserid") // mapeia a URL
+    @ResponseBody/*Descrição da resposta*/
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "idUser") Long idUser) {
+    	
+       Usuario usuario = usuarioRepository.findById(idUser).get();
+       
+       return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+    
+    @PutMapping(value = "atualizar") // mapeia a URL
+    @ResponseBody/*Descrição da resposta*/
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+    
+    	if(usuario.getId() == null) {
+    		
+    		return new ResponseEntity<String>("Id não foi Informado", HttpStatus.OK);
+    		
+    	}
+       Usuario user = usuarioRepository.saveAndFlush(usuario);
+       
+       return new ResponseEntity<Usuario>(user, HttpStatus.OK);
     }
 }
