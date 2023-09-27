c737a3f144dcc10fc62d1568a36d35d7f531f163
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-09-27 11:05:53 -0300

    Definindo o contexto da aplicação

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -23,76 +23,77 @@ import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
 @RestController
 public class GreetingsController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-    @RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Curso jdev treinamentos:  " + name + "!";
-    }
-    
-    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String retonaOlaMundo(@PathVariable String nome) {
-    	
-    	Usuario usuario = new Usuario();
-    	usuario.setNome(nome);
-    	
-    	usuarioRepository.save(usuario);
-    	
-    	return "Ola mundo " + nome;
-    }
-    
-    @GetMapping(value = "/listartodos")
-    @ResponseBody /*Retorna os dados para o corpo da resposta*/
-    public ResponseEntity<List<Usuario>> listaUsuario() {
-    	
-    	List<Usuario> usuarios = usuarioRepository.findAll();
-    	
-    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
-    }
-    
-    @PostMapping(value = "salvar") // mapeia a URL
-    @ResponseBody/*Descrição da resposta*/
-    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
-    	
-       Usuario user = usuarioRepository.save(usuario);
-       
-       return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
-    }
-    
-    @DeleteMapping(value = "delete") // mapeia a URL
-    @ResponseBody/*Descrição da resposta*/
-    public ResponseEntity<String> delete(@RequestParam Long idUser) {
-    	
-       usuarioRepository.deleteById(idUser);
-       
-       return new ResponseEntity<String>("User Deletado com Sucesso", HttpStatus.OK);
-    }
-    
-    
-    @GetMapping(value = "buscaruserid") // mapeia a URL
-    @ResponseBody/*Descrição da resposta*/
-    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "idUser") Long idUser) {
-    	
-       Usuario usuario = usuarioRepository.findById(idUser).get();
-       
-       return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
-    }
-    
-    @PutMapping(value = "atualizar") // mapeia a URL
-    @ResponseBody/*Descrição da resposta*/
-    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
-    
-    	if(usuario.getId() == null) {
-    		
-    		return new ResponseEntity<String>("Id não foi Informado", HttpStatus.OK);
-    		
-    	}
-       Usuario user = usuarioRepository.saveAndFlush(usuario);
-       
-       return new ResponseEntity<Usuario>(user, HttpStatus.OK);
-    }
+
+	@RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Curso jdev treinamentos:  " + name + "!";
+	}
+
+	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String retonaOlaMundo(@PathVariable String nome) {
+
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+
+		usuarioRepository.save(usuario);
+
+		return "Ola mundo " + nome;
+	}
+
+	@GetMapping(value = "/listartodos")
+	@ResponseBody /* Retorna os dados para o corpo da resposta */
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+
+		List<Usuario> usuarios = usuarioRepository.findAll();
+
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "salvar") // mapeia a URL
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+
+		Usuario user = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(value = "delete") // mapeia a URL
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<String> delete(@RequestParam Long idUser) {
+
+		usuarioRepository.deleteById(idUser);
+
+		return new ResponseEntity<String>("User Deletado com Sucesso", HttpStatus.OK);
+	}
+
+	@GetMapping(value = "buscaruserid") // mapeia a URL
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "idUser") Long idUser) {
+
+		Usuario usuario = usuarioRepository.findById(idUser).get();
+
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	}
+
+	@PutMapping(value = "atualizar") // mapeia a URL
+	@ResponseBody /* Descrição da resposta */
+	public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+
+		if (usuario.getId() == null) {
+
+			return new ResponseEntity<String>("Id não foi Informado", HttpStatus.OK);
+
+		}
+
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+	}
+
 }
--- src/main/java/br/com/springboot/curso_jdev_treinamento/repository/UsuarioRepository.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/repository/UsuarioRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
